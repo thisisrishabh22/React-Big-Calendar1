e1f4e8d171512883319cd9daedc83dc1caca8f01
Rishabh Singh <ris8645208@gmail.com>
2021-09-02 18:54:18 +0530

    fix: undefined resolved

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
-import { Calendar, momentLocalizer  } from 'react-big-calendar' 
-import 'react-big-calendar/lib/css/react-big-calendar.css';
-import moment from 'moment';
+import { Calendar, momentLocalizer } from "react-big-calendar";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import moment from "moment";
 
 // Setup the localizer by providing the moment (or globalize) Object
 // to the correct localizer.
-const localizer = momentLocalizer(moment) // or globalizeLocalizer
+const localizer = momentLocalizer(moment); // or globalizeLocalizer
 
 const MyCalendar = () => (
   <div>
     <Calendar
+      style={{ flex: 1 }}
       localizer={localizer}
       events={[]}
       startAccessor="start"
       endAccessor="end"
+      defaultView="month"
     />
   </div>
-)
+);
 
 export default MyCalendar;
