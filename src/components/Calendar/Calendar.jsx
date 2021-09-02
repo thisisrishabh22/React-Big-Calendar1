c844b2b138c9e32fb37e98ffdcb840d6f61c0147
Rishabh Singh <ris8645208@gmail.com>
2021-09-02 18:14:57 +0530

    feat: add calendar to components

---
+++ src/components/Calendar/Calendar.jsx
@@ -0,0 +1,20 @@
+import BigCalendar from 'react-big-calendar'
+import moment from 'moment'
+
+// Setup the localizer by providing the moment (or globalize) Object
+// to the correct localizer.
+const localizer = BigCalendar.momentLocalizer(moment) // or globalizeLocalizer
+
+const Calendar = () => {
+    return (
+        <div>
+            <BigCalendar
+                localizer={localizer}
+                events={myEventsList}
+                startAccessor="start"
+                endAccessor="end"
+            />
+        </div>
+    );
+}
+export default Calendar;
